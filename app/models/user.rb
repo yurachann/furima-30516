a31fa983d1bc4853b960d304825b8002d762171a
yurachann <motomu1220@gmail.com>
2020-10-22 18:55:53 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,         presence: true
+  validates :family_name,      presence: true,format: { with:/\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name,       presence: true,format: { with:/\A[ぁ-んァ-ン一-龥]/}
+  validates :family_name_read, presence: true,format: { with:/\A[ァ-ヶー－]+\z/}
+  validates :first_name_read,  presence: true,format: { with:/\A[ァ-ヶー－]+\z/}
+  validates :birth_date,       presence: true
 end
