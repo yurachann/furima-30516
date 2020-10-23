a581629a2d3adaef4414ebbad92b0f350c9eb6c9
yurachann <motomu1220@gmail.com>
2020-10-23 16:18:07 +0900

    共通のバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,         presence: true
-  validates :family_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :family_name_read, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_read,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_date,       presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :family_name,      format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name,       format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :family_name_read, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_read,  format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birth_date
+  end
 end
