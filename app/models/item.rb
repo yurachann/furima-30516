66419d829ffc0caf11d5cd3642652a0cc123b23a
yurachann <motomu1220@gmail.com>
2020-10-26 15:11:17 +0900

    商品モデルのアソシエーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :shipment_source
+  belongs_to_active_hash :shipment_date
+  
   belongs_to       :user
   has_one_attached :image
+  
 end
