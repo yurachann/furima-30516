8d4266ed90a8ee09373a5be134f51258dd0a352a
yurachann <motomu1220@gmail.com>
2020-10-26 15:21:05 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,8 @@ class Item < ApplicationRecord
   
   belongs_to       :user
   has_one_attached :image
-  
+
+  validates :title, :explain, :category, :condition, :shipping_charge, :shipment_source,:shipment_date, :user, price, presence: true
+  validates :category_id, :condition_id, :shipping_charge_id, :shipment_source_id, :shipment_date_id, numericality: { other_than: 1 } 
+
 end
