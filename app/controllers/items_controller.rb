ae84c39001a4731f88acdcb3c140dab62b240ea3
yurachann <motomu1220@gmail.com>
2020-10-26 17:47:06 +0900

    商品の情報保存機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    binding.pry
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit( :image, :title, :explain, :category_id, :condition_id, :shipping_charge_id, :shipment_source_id, :shipment_date_id, :price).merge(user_id: current_user.id)
+  end
 end
