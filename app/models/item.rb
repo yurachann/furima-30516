a10c510b5af9cfd762784e48a17f79bd0dcabdf0
yurachann <motomu1220@gmail.com>
2020-10-26 18:21:05 +0900

    バリデーションの再設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
   belongs_to       :user
   has_one_attached :image
 
-  validates :title, :explain, :category, :condition, :shipping_charge, :shipment_source,:shipment_date, :user, :price, presence: true
+  validates :title, :explain, :category, :condition, :shipping_charge, :shipment_source,:shipment_date, :user, :price, :image, presence: true
   validates :category_id, :condition_id, :shipping_charge_id, :shipment_source_id, :shipment_date_id, numericality: { other_than: 1 } 
-
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
