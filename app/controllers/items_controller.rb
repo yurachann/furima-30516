020cd52eb46f86247ea939fa759c1e72049113c0
yurachann <motomu1220@gmail.com>
2020-10-27 21:29:41 +0900

    コードの省略

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
+ 
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -18,16 +20,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to action: :index unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -40,4 +39,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :title, :explain, :category_id, :condition_id, :shipping_charge_id, :shipment_source_id, :shipment_date_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
