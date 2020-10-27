79aaaa3ea708e15f7c56f0ec97f4adbaec1fffe8
yurachann <motomu1220@gmail.com>
2020-10-27 21:10:10 +0900

    urlからのアクセスを制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,7 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    redirect_to action: :index unless current_user.id == @item.user_id
   end
 
   def update
